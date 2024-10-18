29ca32c1f3e7e27509d95892f785b46c9768da0b
unknown <Emanuel>
2024-10-18 16:12:28 -0400

    update

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.AWS_REGION
+  region = var.PRIMARY_AWS_REGION
 }
 
 resource "aws_kms_key" "encryption_key" {
@@ -8,15 +8,22 @@ resource "aws_kms_key" "encryption_key" {
   rotation_period_in_days = var.KEY_ROTATION_SCHEDULE
 }
 
-resource "aws_s3_bucket" "root-s3-bucket" {
-  bucket = "${var.BUCKET_NAME}-${var.AWS_REGION}"
+resource "aws_s3_bucket" "primary-s3-bucket" {
+  bucket = "${var.BUCKET_NAME}-${var.PRIMARY_AWS_REGION}"
   tags = {
     Name = var.APP_NAME
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.root-s3-bucket.id
+resource "aws_s3_bucket" "backup-s3-bucket" {
+  bucket = "${var.BUCKET_NAME}-${var.BACKUP_AWS_REGION}"
+  tags = {
+    Name = var.APP_NAME
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "primary-s3-bucket-encryption" {
+  bucket = aws_s3_bucket.primary-s3-bucket.id
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.encryption_key.arn
@@ -25,9 +32,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   }
 }
 
-resource "aws_cognito_user_pool" "pool" {
-  name = "${var.APP_NAME}-USERS-${var.AWS_REGION}"
-  tags = {
-    Name = var.APP_NAME
+resource "aws_s3_bucket_server_side_encryption_configuration" "backup-s3-bucket-encryption" {
+  bucket = aws_s3_bucket.backup-s3-bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.encryption_key.arn
+      sse_algorithm     = var.KEY_ALGORITHM
+    }
   }
 }
