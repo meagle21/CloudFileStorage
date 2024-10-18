835f1aafe139b2ebc6d167302186e1cc5882528b
unknown <Emanuel>
2024-10-18 11:36:49 -0400

    update

--- infra/main.tf
+++ infra/main.tf
@@ -7,9 +7,9 @@
 #   }
 # }
 
-# provider "aws" {
-#   region = var.AWS_REGION
-# }
+provider "aws" {
+  region = var.AWS_REGION
+}
 
 resource "aws_s3_bucket" "root-s3-bucket" {
   bucket = "${var.BUCKET_NAME}-${var.AWS_REGION}"
@@ -18,18 +18,18 @@ resource "aws_s3_bucket" "root-s3-bucket" {
   }
 }
 
-# resource "aws_kms_key" "encryption_key" {
-#   description             = "This key is used to encrypt bucket objects"
-#   enable_key_rotation = true
-#   rotation_period_in_days = var.KEY_ROTATION_SCHEDULE
-# }
+resource "aws_kms_key" "encryption_key" {
+  description             = "This key is used to encrypt bucket objects"
+  enable_key_rotation = true
+  rotation_period_in_days = var.KEY_ROTATION_SCHEDULE
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-#   bucket = aws_s3_bucket.root-s3-bucket.id
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       kms_master_key_id = aws_kms_key.encryption_key.arn
-#       sse_algorithm     = var.KEY_ALGORITHM
-#     }
-#   }
-# }
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.root-s3-bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.encryption_key.arn
+      sse_algorithm     = var.KEY_ALGORITHM
+    }
+  }
+}
