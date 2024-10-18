ea7b74d30736193fe4d72239ed27f15bd2f4b580
unknown <Emanuel>
2024-10-18 12:38:53 -0400

    update

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "Eagle_Industries"  # Replace with your Terraform Cloud organization
-
-    workspaces {
-      name = "CloudFileStorage"  # Replace with your Terraform Cloud workspace
-    }
-  }
-}
